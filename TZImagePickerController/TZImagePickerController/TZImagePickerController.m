7ad91bfcc841681ff8d2d892da20fe630d212569
MagicAna <2273703684@qq.com>
2019-11-29 11:42:22 +0800

    修改发送按钮颜色

--- TZImagePickerController/TZImagePickerController/TZImagePickerController.m
+++ TZImagePickerController/TZImagePickerController/TZImagePickerController.m
@@ -57,7 +57,7 @@
 
     // Default appearance, you can reset these after this method
     // 默认的外观，你可以在这个方法后重置
-    self.oKButtonTitleColorNormal   = [UIColor colorWithRed:(83/255.0) green:(179/255.0) blue:(17/255.0) alpha:1.0];
+    self.oKButtonTitleColorNormal   = [UIColor colorWithRed:(54/255.0) green:(181/255.0) blue:(157/255.0) alpha:1.0];
     self.oKButtonTitleColorDisabled = [UIColor colorWithRed:(83/255.0) green:(179/255.0) blue:(17/255.0) alpha:0.5];
     
     self.navigationBar.barTintColor = [UIColor colorWithRed:(34/255.0) green:(34/255.0)  blue:(34/255.0) alpha:1.0];
